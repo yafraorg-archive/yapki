e2d2ff6a13976365706578dd8d79699776f0caff
Martin Weber Nissle <webernissle@gmail.com>
2020-06-08 09:32:08 +0200

    wip

--- server-admin/control/certlist.py
+++ server-admin/control/certlist.py
@@ -6,23 +6,34 @@ from model.certificate import Certificate
 
 def certlist(filename: str) -> List[Certificate]:
     certs = List[Certificate]
+    certlist = certs()
     with open(filename, 'r') as infile:
         for line in infile:
-            certEntry = {}
             newline = line.rstrip('\n')
             certFields = newline.split('\t')
+            if certFields[3] and not certFields[3].isspace():
+                mySerial = certFields[3]
+            else:
+                # the string is empty
+                mySerial = "empty"
+
+            # certificate date's
             # decode_time(certFields[2], "%y%m%d%H%M%SZ")
-            myRevdate = certFields[2][:-1]
+            if certFields[2] and not certFields[2].isspace():
+                myRevdate = certFields[2][:-1]
+            else:
+                # the string is empty
+                myRevdate = "0"
             myExpdate = certFields[1][:-1]
             certificate = Certificate(id=1, type=certFields[0], expdate=int(myExpdate),
-                                      revdate=int(myRevdate), serial=int(certFields[3]),
-                                      file=certFields[4], name=certFields[4], common_name="test", valid=True,
-                                      description="test", owner_id=1)
+                                      revdate=int(myRevdate), serial=mySerial,
+                                      file=certFields[4], common_name=certFields[4], valid=True,
+                                      description="test", owner_id=1, email="test")
             # CN=(.+?)/
             # emailAddress=(.+?)$ or \\n on the end
-            cn = re.search("CN=(.+?)/", certificate.name)
-            certificate.common_name = cn.group(1)
-            cn = re.search("emailAddress=(.+?)$", certificate.name)
-            certificate.email = cn.group(1)
-            certs.append(certificate)
-    return certs
+            #cn = re.search("CN=(.+?)/", certFields[4])
+            #certificate.common_name = cn.group(1)
+            #cn = re.search("emailAddress=(.+?)$", certFields[4])
+            #certificate.email = cn.group(1)
+            certlist.append(certificate)
+    return certlist
--- server-admin/model/certificate.py
+++ server-admin/model/certificate.py
@@ -7,9 +7,10 @@ class Certificate(BaseModel):
     type: str
     expdate: int
     revdate: int
-    serial: int
+    serial: str
     file: str
     common_name: str
+    email: str
     description: str = None
     valid: bool
     owner_id: int
