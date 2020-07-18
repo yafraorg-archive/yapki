1867ec40a0614ca16114a9a3e177e3dbc85db324
Martin Weber Nissle <webernissle@gmail.com>
2020-07-18 15:02:04 +0200

    tested initial python script to init db

--- server-user/src/certificate/certificate.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('certificate')
-export class CertificateController {}
--- server-user/src/certificate/certificate.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CertificateController } from './certificate.controller';
-import { CertificateService } from './certificate.service';
-
-@Module({
-  controllers: [CertificateController],
-  providers: [CertificateService]
-})
-export class CertificateModule {}
